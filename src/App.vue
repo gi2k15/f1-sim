955268da8896b6b4d4273eb3e96739b9325fe1f0
gi2k15 <gendouikari2015@gmail.com>
2025-08-18 18:10:59 -0300

    Importação do JSON e tabela para edições

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const tabelaPilotos = ref([])
+
+function getJSON() {
+  const textoJSON = document.getElementById('json-pilotos').value
+  try {
+    tabelaPilotos.value = JSON.parse(textoJSON).slice(0, 20)
+  } catch (e) {
+    alert("Erro: " + e.message)
+  }
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <form id="importar-pilotos">
+    <textarea id="json-pilotos"></textarea>
+    <button @click.prevent="getJSON()">Importar</button>
+  </form>
+  <form id="editar-pilotos">
+    <div v-for="p in tabelaPilotos" :key="p.pontuacao" class="pilotos">
+      <label>{{ p.nome }}</label>
+      <input />
+    </div>
+  </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+#importar-pilotos {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+}
+
+#importar-pilotos textarea {
+  width: 300px;
+  height: 200px;
+}
+
+#editar-pilotos {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  gap: 5px;
+}
+
+.pilotos {
+  display: flex;
+  flex-direction: column;
+}
+</style>
