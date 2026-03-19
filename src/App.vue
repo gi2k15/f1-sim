c8972d92dabeffdd8e1d8af4f17431b5154a4280
gi2k15 <gendouikari2015@gmail.com>
2026-03-19 18:15:50 -0300

    Setup inicial

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <v-app>
-    <v-app-bar title="F1 Championship Predictor">
-      
+    <v-app-bar>
+      <v-app-bar-title> Quem ganhará na Fórmula 1? </v-app-bar-title>
     </v-app-bar>
     <v-main>
       <v-container>
-        <p>Teste</p>
-        <div class="text-center">
-          <v-btn text="Importar pontuação" class="p-center">
-          </v-btn>
-        </div>
+        <v-row>
+          <v-col>
+            <p>Clique no botão abaixo para importar a pontuação atual.</p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-title> Configurações </v-expansion-panel-title>
+              <v-expansion-panel-text>
+                <v-container>
+                  <v-row>
+                    <v-col>
+                      <v-number-input
+                        :min = 0
+                        control-variant="split"
+                        label="Número de corridas"
+                      ></v-number-input>
+                    </v-col>
+                    <v-col>
+                      <v-number-input
+                        :min = 0
+                        control-variant="split"
+                        label="Número de sprints"
+                      ></v-number-input>
+                    </v-col>
+                    <v-col>
+                      <v-number-input
+                        :min = 0
+                        control-variant="split"
+                        label="Número de simulações"
+                      ></v-number-input>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-expansion-panel-text>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-row>
+        <v-row>
+          <v-col class="text-center">
+            <v-btn>Importar pontuação</v-btn>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
-
-<script setup>
-</script>
