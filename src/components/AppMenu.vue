7361c2303a9f7c6d96385243ada292b4469f6552
gi2k15 <gendouikari2015@gmail.com>
2026-04-07 20:33:53 -0300

    Adiciona página FAQ e atualiza o roteamento

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -17,7 +17,8 @@ const theme = useTheme();
         "
         @click="theme.cycle()"
       ></v-btn>
-      <v-btn icon="mdi-home" to="/"></v-btn>
+      <v-btn icon="mdi-home" to="/" />
+      <v-btn icon="mdi-frequently-asked-questions" to="/faq" />
     </template>
   </v-app-bar>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/pages/Home.vue";
+import FAQ from "@/pages/FAQ.vue";
 
-const routes = [{ path: "/", component: Home }];
+const routes = [
+  { path: "/", component: Home },
+  { path: "/faq", component: FAQ },
+];
 
 const router = createRouter({
   history: createWebHistory(),
