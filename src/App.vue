2ea5ea2aa9466954c76ef7046174d85446313a9c
gi2k15 <gendouikari2015@gmail.com>
2026-04-08 18:55:09 -0300

    Alteração para padronização

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,18 @@
     <AppMenu @open-drawer="isOpened = !isOpened" />
     <NavBar v-model="isOpened" />
     <v-main>
-      <router-view />
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
 import AppMenu from "@/components/AppMenu.vue";
-import NavBar from "./components/NavBar.vue";
+import NavBar from "@/components/NavBar.vue";
 
 const isOpened = defineModel({ type: Boolean, default: false});
 </script>
