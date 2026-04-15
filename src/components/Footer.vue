7a887f4509fe7cb413c8fb5e81b7b5ed27a4d299
gi2k15 <gendouikari2015@gmail.com>
2026-04-15 20:45:52 -0300

    Adiciona tooltip ao botão do GitHub no componente Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,10 @@
   <v-footer class="d-flex align-center justify-center">
     <v-btn
       icon="mdi-github"
-      color=""
+      v-tooltip="'GitHub'"
       href="https://github.com/gi2k15/f1-sim"
       target="_blank"
+      max-height="50"
     />
   </v-footer>
 </template>
