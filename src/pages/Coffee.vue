82778b495b8cd9a5dab4e0f90cf75ae0d4e03cea
gi2k15 <gendouikari2015@gmail.com>
2026-04-19 11:24:39 -0300

    Melhora a estrutura do componente Coffee, adicionando layout responsivo e elementos visuais

--- src/pages/Coffee.vue
+++ src/pages/Coffee.vue
@@ -1,7 +1,22 @@
 <template>
-  <v-container class="mt-10">
-    <v-sheet rounded>
-      <div class="text-display-large text-center">OBRIGADO!</div>
-    </v-sheet>
+  <v-container class="mt-10 fill-height">
+    <v-row class="flex-column">
+      <v-col
+        ><div class="text-display-large text-center">
+          ☕ OBRIGADO! 🥖
+        </div></v-col
+      >
+      <v-col
+        ><div class="text-headline-small text-grey">
+          Sua contribuição significa muito para mim!
+        </div></v-col
+      >
+      <v-col class="mt-10">
+        <v-sheet class="pa-4" rounded
+          ><div class="text-title-large text-center">PIX</div>
+          <v-img src="/pix.jpg" max-width="200px" class="mt-5 ma-auto"> </v-img>
+        </v-sheet>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
